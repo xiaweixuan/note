8aabd3d34fd6a1822f721e73471b02bda289c58b
夏炜轩 <37130273+xiaweixuan@users.noreply.github.com>
2021-08-30 21:59:36 +0800

    Update JumpToNote.vue

--- docs/.vuepress/components/JumpToNote.vue
+++ docs/.vuepress/components/JumpToNote.vue
@@ -1,3 +1,13 @@
 <template>
   <p>loading</p>
 </template>
+<script>
+export default {
+  name: "JumpToNote",
+  mounted() {
+    this.$router.push({
+      path: `/note/`,
+    });
+  },
+};
+</script>
