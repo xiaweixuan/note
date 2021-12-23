ea6f1217948d80e8c9d5e1321955a87af32a6446
xiaweixuan <xiaweixuan@bytedance.com>
2021-12-23 10:43:04 +0800

    文章规整

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -23,52 +23,11 @@ module.exports = {
     prevLinks: false,
     nav: [
       { text: '主页', link: 'http://xiawx.top' },
+      { text: 'Todo', link: 'http://todo.xiawx.top' },
       { text: '学习笔记', link: '/note/' },
     ],
     sidebar: {
       '/note/': generateNoteSidebar(),
-      // '/note/': [
-      //   {
-      //     "title": "Applet",
-      //     "path": "/note/Applet/",
-      //     "collapsable": false,
-      //     "children": [
-      //       {
-      //         "title": "小程序开发如何使用colorUI框架",
-      //         "path": "/note/Applet/小程序开发如何使用colorUI框架.md"
-      //       }
-      //     ]
-      //   },
-      //   {
-      //     "title": "js",
-      //     "path": "/note/js/",
-      //     "collapsable": false,
-      //     "children": [
-      //       {
-      //         "title": "ES6",
-      //         "path": "/note/js/ES6.md"
-      //       },
-      //       {
-      //         "title": "babel",
-      //         "path": "/note/js/babel.md"
-      //       },
-      //       {
-      //         "title": "对于js事件的解析",
-      //         "path": "/note/js/对于js事件的解析.md"
-      //       },
-      //       {
-      //         "title": "常见方法封装",
-      //         "path": "/note/js/常见方法封装.md"
-      //       }
-      //     ]
-      //   },
-      //   {
-      //     "title": "社区文章",
-      //     "path": "/note/社区文章/",
-      //     "collapsable": false,
-      //     "children": []
-      //   }
-      // ]
     }
   },
 }
